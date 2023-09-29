5f4b664bbc7925b3ec44aaeb8da2fc8803b2712d
berduk1 <bernard.dukic@gmail.com>
2023-09-29 16:27:52 +0200

    corr.

--- src/components/PortalDemo.js
+++ src/components/PortalDemo.js
@@ -1,14 +1,6 @@
-<<<<<<< HEAD
+
 import React from "react";
 import  ReactDOM  from "react-dom";
-
-function PortalDemo(){
-    return ReactDOM.createPortal(
-        <h1>Portal Demo</h1>, 
-        document.getElementById("portal-root")
-=======
-import React from 'react'
-import  ReactDOM  from 'react-dom';
 
 function PortalDemo(){
     return ReactDOM.createPortal(
@@ -16,7 +8,6 @@ function PortalDemo(){
             Portal Demo
         </h1>, 
         document.getElementById('portal-root')
->>>>>>> 498e7d988f3cd0d1d4acd17962fa049d9aeef24f
     );
 }
 export default PortalDemo
