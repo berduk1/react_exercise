27befa92f366816d37eb1636f01a287a659f23eb
berduk1 <bernard.dukic@gmail.com>
2024-05-20 00:46:21 +0200

    Newcom

--- src/components/UserGreeting.js
+++ src/components/UserGreeting.js
@@ -4,7 +4,7 @@ class UserGreeting extends Component{
     constructor(props){
         super(props)
         this.state = {
-            isLoggedIn: true
+            isLoggedIn: false
         }
     }
     render(){
